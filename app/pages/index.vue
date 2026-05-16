cd48cce0d0d290ee34c71a8c29f4089beaad78e0
Serhii <tustanovskiis@gmail.com>
2026-05-16 12:47:11 +0200

    feat: get products

---
+++ app/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Pizza App</h1>
+    <p>Welcome to the Pizza App!</p>
+  </div>
+</template>
+<script lang="ts" setup>
+  import { AppSuccess } from '~/types/app';
+  import type { Product } from '~/types/product';
+
+  const {
+    $api: { product },
+  } = useNuxtApp();
+
+  const { data } = await useAsyncData('products', async () => {
+    const response = await product.getAll();
+    if (response instanceof AppSuccess) {
+      return {
+        items: response.data as Product[],
+        total: Number(response.headers.get('x-total-count') ?? 0),
+        offset: Number(response.headers.get('x-total-pages') ?? 1),
+      };
+    }
+    throw response;
+  });
+
+  const products = computed(() => data.value?.items ?? []);
+  const total = computed(() => data.value?.total ?? 0);
+  const offset = computed(() => data.value?.offset ?? 0);
+</script>
