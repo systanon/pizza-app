15ff539f510f7f01f1b0a8849f1435236c3570aa
Serhii <tustanovskiis@gmail.com>
2026-05-20 14:15:52 +0200

    feat: add ProductCard component

---
+++ app/components/ProductCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card>
+    <v-img :src="product.image_url" :alt="product.name" height="300" cover>
+      <template #placeholder>
+        <div class="d-flex align-center justify-center fill-height">
+          <v-progress-circular indeterminate color="primary" />
+        </div>
+      </template>
+
+      <template #error>
+        <v-img
+          class="mx-auto"
+          height="300"
+          src="https://picsum.photos/500/300?image=232"
+          cover
+        ></v-img>
+      </template>
+    </v-img>
+    <v-card-title>{{ product.name }}</v-card-title>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="primary">Order Now</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script setup lang="ts">
+  import type { Product } from '~/types/product';
+  defineProps<{
+    product: Product;
+  }>();
+</script>
